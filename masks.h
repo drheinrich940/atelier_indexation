ea4c83863c4597718077db4424da2fb8c1071649
amugnier <amugniere@gmail.com>
2019-09-23 18:53:29 +0200

    :sparkles: averaging filter added

--- masks.h
+++ masks.h
@@ -11,16 +11,21 @@ const int impulse_response[3][3] = {
         {1,1,1}
 };
 
-const int horizontal_gradiant[3][3] = {
+const int horizontal_gradient[3][3] = {
         {-1,0,1},
         {-2,0,2},
         {-1,0,1}
 };
 
-const int vertical_gradiant[3][3] = {
+const int vertical_gradient[3][3] = {
         {-1,-2,-1},
         {0,0,0},
         {1,2,1}
 };
 
+const int averaging_filter[3][3] = {
+        {1/9,1/9,1/9},
+        {1/9,1/9,1/9},
+        {1/9,1/9,1/9}
+};
 #endif //ATELIER_INDEXATION_MASKS_H

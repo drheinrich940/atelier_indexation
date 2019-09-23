959490e35a8093622204979476a149afb2d8ca48
Henri <drheinrich940@gmail.com>
2019-09-23 23:18:24 +0200

    :pencil: explained the functions in functions.h

--- functions.h
+++ functions.h
@@ -7,6 +7,12 @@
 
 #include "nrc/def.h"
 
+/*
+ * PrintMask allows you to print the selected mask in the console. yeepee !
+ * VerifyMask allows you to post process and verify that the value from the mask convolution is correct.
+ * ApplyMastToMatrix applies the selected 3*3 mask to the targetted matrix.
+ */
+
 void printMask(int mask[3][3]);
 int verifyRGBValue(int value);
 void applyMaskToMatrix(int mask[3][3], byte **matrix, byte **outputMatrix, int matrix_max_x, int matrix_max_y);
